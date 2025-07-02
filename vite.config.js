1a71b41543b1b2203ff8a98ed2cb93e426a53113
JoeyZandvliet <jzandvliet@swis.nl>
2025-07-02 15:48:42 +0200

    feat: add ddev

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,33 @@ import vue from '@vitejs/plugin-vue';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+    resolve: {
+        alias: {
+            '@fonts': '/resources/fonts',
+        },
+    },
+    server: process.env.IS_DDEV_PROJECT ? {
+        strictPort: true,
+        host: true,
+        hmr: {
+            host: process.env.DDEV_HOSTNAME.split(',')[0],
+            protocol: 'wss',
+        },
+    } : {},
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
+            publicDirectory: 'public',
             refresh: true,
         }),
-        vue(),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
         tailwindcss(),
     ],
 });
